6578f59d1fba6791352a25e899dbfdf6636260c1
sarah <sarah.sirhandi@gmail.com>
2022-08-16 14:20:48 -0400

    Initial Commit

---
+++ pocketguidetopalestine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pocketguidetopalestine
+//
+//  Created by Sarah Sirhandi on 8/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
