df65c36b1a65ddd45f9d02abd71d730b7251be27
MrPalRobot <kh.abbasfatima@gmail.com>
2022-08-19 08:09:07 +0300

    New changes including UN

--- pocketguidetopalestine/KhadeejaFirstViewController.swift
+++ pocketguidetopalestine/KhadeejaFirstViewController.swift
@@ -15,6 +15,30 @@ class KhadeejaFirstViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    //news article button for Gaza
+    @IBAction func newsArticle1(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string: "https://www.aljazeera.com/news/2022/8/12/gaza-death-toll-rises-to-49-including-17-children")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    //Link to podcast about child
+    @IBAction func lifeAndDeathChild(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string: "https://www.aljazeera.com/podcasts/2022/1/3/best-of-the-take-the-life-and-death-of-one-palestinian-child")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    
+    //Link to UN special rapporteur
+    
+    @IBAction func uNButton(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string: "https://www.aljazeera.com/news/liveblog/2022/8/7/israel-palestine-latest-updates-dozens-killed-in-three-day-gaza-assault")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    
+    
+    
+    
     
     
     
